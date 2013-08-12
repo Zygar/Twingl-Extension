aee14967af9853f5d3513bb309aa953ed3cc4164
Andy Wilkinson <a@andywilkinson.co.nz>
2013-08-13 11:54:47 +1200

    Adds test plugin which integrates another field to annotator.

--- twingl.js
+++ twingl.js
@@ -1,1 +1,22 @@
-$(document.body).annotator();
+Annotator.Plugin.HelloWorld = (function() {
+
+  function HelloWorld(element, options) {
+    this.element = element;
+    this.options = options;
+    console.log("Hello World!");
+  }
+
+  HelloWorld.prototype.pluginInit = function() {
+    console.log("Initialized with annotator: ", this.annotator);
+    this.annotator.editor.addField({
+      label: 'Test',
+      load: function(field, annotation) {
+        
+      }
+    })
+  };
+
+  return HelloWorld;
+})();
+
+$(document.body).annotator().annotator('addPlugin', 'HelloWorld');
