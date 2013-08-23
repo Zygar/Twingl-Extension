fe1275a34089f5d2aa9ec4ff3b945065c8d6babc
Andy Wilkinson <a@andywilkinson.co.nz>
2013-08-23 12:26:17 +1200

    Improves documentation and just generally cleans up our content script.

--- twingl_content.js
+++ twingl_content.js
@@ -9,11 +9,14 @@ chrome.runtime.sendMessage({
   console.log(response.token);
   var token = response.token;
 
+  // Set up authentication headers.
   $.ajaxSetup({
     headers: {
       'Authorization': 'Bearer ' + token
     }
   });
+
+  // Get all highlights. (For the Twingler)
   $.ajax({
     url: "http://api.twin.gl/flux/highlights?context=twingl://mine",
     type: "GET",
@@ -22,6 +25,7 @@ chrome.runtime.sendMessage({
     }
   });
 
+  // Initialise Annotator
   $(document.body).annotator().annotator('addPlugin', 'Synapses').annotator('addPlugin', 'Auth', {
     token: response.token
   }).annotator('addPlugin', 'Store', {
@@ -32,48 +36,51 @@ chrome.runtime.sendMessage({
       update: 'highlights/:id',
       destroy: 'highlights/:id'
     }
-  }).append("<div id='synapser'><ul></ul></div>");
+  }).append("<div id='synapser'><ul></ul></div>"); // Construct the Synapser UI Element
+
+  // Continue constructing and binding Synapser UI element.
   $('#synapser').append("<button id='synapser-close'>Close</button>");
   $('#synapser-close').click(function() {
     $("#synapser").toggleClass("visible");
   });
 
+  // Inject highlights into the Synapser
+
   function renderHighlightsList(data) {
     for (var i = data.length - 1; i >= 0; i--) {
       current = data[i];
-      //console.log(current);
       $("#synapser ul").append("<li class='retrieved-highlight' data-id=" + current.id + ">" + current.quote + "</li>")
     };
-    $('.retrieved-highlight').click(function() {
-      console.log("Click!");
-      // $(this).toggleClass("synapse-selected")
-    });
   }
 });
 
+
+/* Initialise the Synapser. It is passed the ID of the Highlight from which "Synapser" was invoked.*/
+
 function initSynapser(active_highlight_id) {
   console.log("Woo! We've initialised the synapser with ID " + active_highlight_id);
-  var currentTwinglings = [];
 
+  var currentTwinglings = [];
   $synapser = $("#synapser");
   $highlights = $("#synapser ul li");
   $synapser.addClass("visible");
 
-  /* Retrieve Twinglings.
-     This is somewhat blocking.  */
+
+  /** Retrieve Twinglings for "Active" highlight.
+   * We don't want any interaction during this stage, so block with some CSS and display throbber.
+   * We retrieve all Twinglings attached to the highlight from which
+   * the Twingler was initialised. (active_highlight_id)
+   * Then, we stuff start_id/end_id into a one-dimensional array: dropping
+   * any ID which matches the current ID.
+   * Finally, we initialise the "Highlight Checker"
+   **/
+
   $synapser.addClass("working");
+
   $.ajax({
     url: "http://api.twin.gl/flux/highlights/" + active_highlight_id + "/twinglings",
     type: "GET",
     success: function(data) {
-      /* We retrieve all Twinglings attached to the highlight from which
-         the Twingler was initialised. (active_highlight_id)
-
-         Then, we stuff start_id/end_id into a one-dimensional array: dropping
-         any ID which matches the current ID.
-
-         Finally, we initialise the "Highlight Checker" */
-      var allTwinglings = data;
       for (var i = data.length - 1; i >= 0; i--) {
         var currentTwingling = {
           twingling_id: data[i].id,
@@ -81,10 +88,8 @@ function initSynapser(active_highlight_id) {
         }
         if (data[i].end_id != active_highlight_id) {
           currentTwingling.dest_id = data[i].end_id;
-
         } else if (data[i].start_id != active_highlight_id) {
           currentTwingling.dest_id = data[i].start_id;
-          
         } else {
           console.error("If this fires, something has gone seriously wrong. I'm not sure what that could be.")
         }
@@ -95,20 +100,20 @@ function initSynapser(active_highlight_id) {
     }
   });
 
-  
+
   /* This is the "Event Binding" loop. It goes through each returned highlight
      in the DOM, greys out the current highlight and "greens up" the active Twinglings.
-     It also binds up the events, naturally. */ 
+     It also binds up the events, naturally. */
 
   function checkHighlights(currentTwinglings) {
     $highlights.each(function(i) {
       var $element = $(this);
-      var current_dom_id = $element.data("id"); // Cache ID of each highlight in the list.
+      var current_dom_id = $element.data("id"); // Cache ID of each highlight through which we iterate.
       if (current_dom_id == active_highlight_id) {
         setState.active($element);
       } else if (currentTwinglings.length > 0) {
         setState.twinglable($element, active_highlight_id, current_dom_id);
-        for (var i = currentTwinglings.length - 1; i >= 0; i--) { 
+        for (var i = currentTwinglings.length - 1; i >= 0; i--) {
           /* We loop through a list of all Twinglings associated with the current highlight.
              If any of them match the current DOM element in jQuery's $.each loop, we change its status to "Twingled".*/
           if (currentTwinglings[i].dest_id == current_dom_id) {
@@ -139,9 +144,9 @@ var setState = {
   active: function(elem) {
     elem.off("click");
     elem.attr('class', 'current');
-  }, 
+  },
   working: function(elem) {
-    elem.addClass("working"); 
+    elem.addClass("working");
     elem.off("click");
   }
 }
@@ -177,10 +182,9 @@ var modifyTwingling = {
   },
   greatSuccess: function(elem, active_highlight_id, current_dom_id, type, twingling_id) {
     elem.removeClass("working");
-    if(type === "destroy") {
+    if (type === "destroy") {
       setState.twinglable(elem, active_highlight_id, current_dom_id)
-    }
-    else if (type === "create") {
+    } else if (type === "create") {
       setState.twingled(elem, active_highlight_id, current_dom_id, twingling_id)
     }
   }
@@ -191,21 +195,6 @@ var updateHighlightList = {
     $("#synapser ul").prepend("<li class='retrieved-highlight' data-id=" + annotation.id + ">" + annotation.quote + "</li>")
   },
   remove: function(annotation) {
-    $("*[data-id="+annotation.id+"]").remove();
+    $("*[data-id=" + annotation.id + "]").remove();
   }
 }
-
-// Probably need update-comment as well.
-
-
-
-/*On page load, "modal" is created and all highlights are loaded in.
-  (If a highlight gets added over the course of this instance, we'll append it. )
-When "Synapse" button is hit, we show the "modal" and pass it the ID of the highlight.
-  This will:
-    -Grey out current highlight (no self synapses)-
-    Turn all highlights that are synapsed-to (or from), green.
-      To do this, we need to:
-        Retrieve /twinglings for current highlight. This will return an array of inbound/outbound Twinglings.
-        Normalise end_id and start_id; filtering out anything which matches current_id
-        Change style of all highlights that match IDs in this list.  */
