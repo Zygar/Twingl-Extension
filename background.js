2fac3c36c051de9826ab9b3ce4389b81ec9dcc47
Andy Wilkinson <a@andywilkinson.co.nz>
2013-08-30 12:41:31 +1200

    Adds logic chain that decides whether to begin ignition sequence.

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 /* On Install */
 // Technically, this gets set on event page load
-var seedStorage = { 
+var seedStorage = {
   paused: false,
   blacklist: {
-    "google.com": true
+    "stackoverflow.com": true
   }
-}; 
+};
 
+// DEV: Reset storage.
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.clear(function(){
     console.log("The extension has been updated. Dumping storage.");
@@ -17,8 +18,9 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 
-/* On Event Page Load */
-// Initialise Variables
+/* $01 – EVENT PAGE INITIALISED */
+
+/* Initialise Variables */
 window.token = null;
 var twingl = new OAuth2('twingl', {
   client_id: '94da4493b8c761a20c1a3b4d532d9ab301745c137b88a574298dc1ebe99d5b14',
@@ -26,16 +28,14 @@ var twingl = new OAuth2('twingl', {
   api_scope: 'private'
 });
 
-// Construct an empty sessionCache construct
-var sessionCache = { 
+// Construct an empty sessionCache object.
+var sessionCache = {
   global_status: null,
-  1066: {
-    state: "blacklisted" // blacklisted | initialised | loading? | do we have global states like paused, signed out? 
-  }
+  tabs: {}
 };
 
 
-// Define Functions
+/* Define functions to run on initialisation */
 function getGlobalState() {
   chrome.storage.sync.get('paused', function(data) {
     if (data.paused === true) {
@@ -56,71 +56,130 @@ function checkAuth() {
   }
 };
 
-// Retrieve global state 
+/* Set State Variable */
 getGlobalState();
 
+/* Retrieve details of current session from localStorage */
+// PENDING.
+
+
+/* $02 – MAIN EVENTS */
+/* Tab Switch */
+chrome.tabs.onActivated.addListener(function(){
+  console.log("Switched to a tab, bro.")
+})
+
+
+/* Tab Update */
+/* This listener fires when a tab starts loading, finishes loading. */
+
+chrome.tabs.onUpdated.addListener(function(id, changeInfo, tab) {
+  if (sessionCache.global_status != "active") {
+    console.log("Stopping. The browser plugin is in this state:", sessionCache.global_status);
+  } else {
+    // Anything in this else block will fire on ANY update event.
+    // Including tab pinning, load and complete.
+    if (changeInfo.status == "loading") {
+      sessionCache.tabs[id] = {
+        url: tab.url, 
+        state: "loading"
+      };
+      console.log("Loading tab", id);
+      console.log(sessionCache.tabs[id]);
+      if (checkBlacklist(tab.url)) {
+        console.log("Shit is blacklisted!");
+        sessionCache.tabs[id].state = "blacklisted";
+      };
+    } else if (changeInfo.status == "complete") {
+      console.log("Tab is loaded", id);
+      if (sessionCache.tabs[id].state == "blacklisted") {
+        // If it's blacklisted, we want to terminate here. 
+        // If it's the active tab, we'll set the icon too. 
+      } else {
+        // ENGAGE LAUNCH SEQUENCE! 
+      }
+    }
+  }
+  
+});
+
+function checkBlacklist(url) {
+  // TODO: Retrieve from storage. 
+  // OTHER TODO: We might need to hardcode exceptions for certain URL patterns. No file:// etc
+  var hostname = getHostname(url);
+  console.log("Checking", hostname); 
+  if (seedStorage.blacklist[hostname] == true) {
+    return true;
+  } else {return false}
+};
+
+function getHostname(url) {
+  var domain = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+  return domain;
+};
+
 /*
-  On Event Page load: 
+  On Event Page load:
   (This stuff happens EVERY TIME event page initialises regardless of state.)
-    1. Retrieve pause status, assign to sessionCache object. 
+    1. Retrieve pause status, assign to sessionCache object.
     2. Check if there's an auth token:
       1.1. If yes, but expired, request a new one, set state to authed.
       1.2. If no, set state to signed out.
-    3. Retrieve session states from the localStorage cache. 
+    3. Retrieve session states from the localStorage cache.
 
-  On tab switch: 
+  On tab switch:
   (switch, new foreground tab. This check will fire BEFORE tab.update)
     0. Check global state. If paused or signed out, do no further checks. [Bind popup to respective action.]
-    1. Check the tab id against session cache object. Does it exist?. 
+    1. Check the tab id against session cache object. Does it exist?.
       1.1. If NO, it's a new foreground tab, or the extension was paused or signed out when this tab was created. (how to handle?)
-      1.2. If YES, check state. Is it loading, initialised or blacklisted? 
-    2. Set icon and change active popup based on the retrieved state. 
+      1.2. If YES, check state. Is it loading, initialised or blacklisted?
+    2. Set icon and change active popup based on the retrieved state.
 
-  On tab update: 
+  On tab update:
   (refresh, new tab(background or foreground), navigate to new page)
-  
-    Upon 'loading' event: 
-    1. Check global state. 
-      1.1. If paused or signed out, execute no further. (N.B. NO icon/popup code here. That is a tab switch concern.) 
-      1.2. Else, set up a listener for the "loaded" event. 
-    2. Create or update sessioncache object. 
-      $tab_id: {
-        url: $tab_url,
-        state: "loading"
-      }
-    3. Retrieve blacklist from chrome storage. Check HOSTNAME against blacklist. 
-      3.1. If exists, set state to "blacklisted". 
+
+    Upon 'loading' event:
+    1. Check global state.
+      ----1.1. If paused or signed out, execute no further. (N.B. NO icon/popup code here. That is a tab switch concern.)
+      ----1.2. Else, set up a listener for the "loaded" event.
+    ---2. Create or update sessioncache object.
+          $tab_id: {
+            url: $tab_url,
+            state: "loading"
+          }
+    3. Retrieve blacklist from chrome storage. ---Check HOSTNAME against blacklist.
+      ---3.1. If exists, set state to "blacklisted".
 
 
     Upon 'loaded' event:
-      1. Check if tab is active, so we know whether to set icon. 
+      1. Check if tab is active, so we know whether to set icon.
       2. Check sessioncache local status. [side note: what happens if the page loads before blacklist is checked?]
-        2.1. If blacklisted, do not inject script, and terminate here. 
-          2.1.1. Set icon to blacklisted, if tab is active. 
+        2.1. If blacklisted, do not inject script, and terminate here.
+          2.1.1. Set icon to blacklisted, if tab is active.
         2.2. If not blacklisted, inject script with auth token passed.
 
-        Upon successful script injection: 
+        Upon successful script injection:
           1. Set sessionCache status to "initialised"
           2. If tab is active, set icon/popups to green. Otherwise, will be set on tab switch event
-          3. Complete!   
+          3. Complete!
 
   OTHER EVENTS:
     On tab close
       Destroy session variable
-  
+
     On browser close
-      Dump entire session cache. 
-  
+      Dump entire session cache.
+
     Popup: Add to blacklist
       Retrieves blacklist from chrome storage.
       Gets the HOSTNAME of the current tab.
       Appends it to list as new object.
       Saves blacklist.
-      Refreshes page. 
-    
+      Refreshes page.
+
     Popup: On pause/unpause
-      Set global pause status. 
-      Write status to chrome.storage. 
+      Set global pause status.
+      Write status to chrome.storage.
 
     Popup: On sign out/in
       Set global status to signed out/in
@@ -128,8 +187,8 @@ getGlobalState();
 
 
     On event page unload:
-      Write current sessionCache object to localStorage. 
-  
+      Write current sessionCache object to localStorage.
+
 
 */
 
@@ -137,35 +196,11 @@ getGlobalState();
 
 
 
-
-/* Authentication Check */
-/* This will fire whenever the event page is called. */
-
-
-
-
-chrome.tabs.onActivated.addListener(function(){
-  console.log("Switched to a tab, bro.")
-})
-
-chrome.tabs.onUpdated.addListener(function(id, changeInfo, tab){
-  console.log("Tab updated", id, changeInfo, tab);
-  if(changeInfo.status == "loading") {
-    console.log("Loading tab")
-  }
-  if(changeInfo.status == "complete") {
-    console.log("Tab is loaded");
-    injectTwingl(id);
-    // Tab has finished loading. 
-    // We now inject the content script if the content script should be injected.
-  }
-});
-
 function injectTwingl(tab_id) {
   chrome.tabs.executeScript(tab_id, {code: "var token = '"+window.token+"'"}, function(){
     chrome.tabs.executeScript(tab_id, {file: 'twingl_content.js'}, function(){
       console.log("Initialised content script.")
-    })   
+    })
   })
 };
 
